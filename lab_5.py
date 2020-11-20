2513caa3190c08c7a01dc374339e11e0f17f2178
turnerjake <jaketurner@mail.fresnostate.edu>
2020-11-19 19:19:10 -0800

    Structured english

---
+++ lab_5.py
@@ -0,0 +1,16 @@
+"""
+Lab 5
+
+Program to translate regular user english into texting shorthand
+Open textese.txt
+Read from the text file and create a list of all translation
+Create a python dictionary of key:value pairs from the list
+Close the file
+
+Take user input in
+Split user input into an array of strings
+
+Loop through array of strings, find a matching key in our dictionary and return the value
+
+Print the translated sentence
+"""
